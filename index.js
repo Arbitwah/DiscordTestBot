a0264c9aa6701b5a5300cef50a3ecb924b1f60c4
GregPughSoftwareProjects <arbitwah@gmail.com>
2019-11-04 15:01:13 -0600

    bot online

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+//console.log(require('dotenv').config());
 require('dotenv').config(); //Dotenv is a zero-dependency module that loads environment variables from a .env file into proccess.env
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const TOKEN = process.evn.TOKEN;
+const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
 
