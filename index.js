aef3a221f69a849e9855904df4290292b308b8b4
Gregory Pugh <arbitwah@gmail.com>
2019-11-14 03:09:31 -0600

    Tester bot

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 //console.log(require('dotenv').config());
-require('dotenv').config(); //Dotenv is a zero-dependency module that loads environment variables from a .env file into proccess.env
+//srequire('dotenv').config(); //Dotenv is a zero-dependency module that loads environment variables from a .env file into proccess.env
+//Gregory Pugh 
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const{prefix, token} = require('./config.json');
+const fs = require('fs');
+const client = new Discord.Client();
+client.command = new Discord.Collection();
+client.once('ready',()=>{
+    console.info('Ready!');
+})
 
-const TOKEN = process.env.TOKEN;
+client.on('message',msg=>{
+    if(!msg.content.startsWith(prefix)|| msg.author.bot) return;
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    console.info(command);
 
-bot.login(TOKEN);
-
-bot.on ('ready',()=>{
-    console.info(`Logged in as ${bot.user.tag}`);
+    if(command==='ping'){
+        msg.channel.send('pong');
+    }else if(command ==='sally'){
+        msg.channel.send('Sells Sea Shells down by the Sea Shore');
+    }
 });
+
+client.on('message',msg=>{
+if(!msg.content.startsWith(prefix)|| msg.author.bot) return;
+const args = msg.content.slice(prefix.length).split(/ +/);
+const command = args.shift().toLowerCase();
+
+if(command === 'tester'){
+    const guildmem = msg.member;
+    guildmem.addRole('---');
+    msg.channel.send(msg.member +' has been added to the tester channel');
+    
+}
+});
+
+
+client.login(token);
